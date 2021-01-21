539992203b58a73e868945382e8a2eaf5a5dcf12
Michael Chan <mijoch@gmail.com>
2021-01-20 16:09:41 -0800

    fixup! add improve this page link

--- .eleventy.js
+++ .eleventy.js
@@ -66,7 +66,7 @@ module.exports = (eleventyConfig) => {
 	});
 
 	eleventyConfig.addFilter('pageSourcePath', function (inputPath) {
-		return `https://github.com/LunchDevCommunity/community-calendar/tree/main/${inputPath.replace('./', '')}`;
+		return `https://github.com/LunchDevCommunity/community-calendar/edit/main/${inputPath.replace('./', '')}`;
 	});
 
 	if (isProduction) {
